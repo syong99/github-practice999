2f252243467ca1621055cd2961c9b8de1eae4cd7
DESKTOP-68QM4EC\hi <sy99639@naver.com>
2024-02-16 09:58:22 +0900

    first commit

---
+++ src/main/java/org/ohgiraffers/githubpractice/GithubPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.githubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ohgiraffers/githubpractice/GithubPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.githubpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
